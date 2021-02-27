d285be68b3e27eea70a2c9ea9a85294d72a93bd5
CairoCoder <cairocoder@gmail.com>
2021-02-27 02:32:48 +0200

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=db3new.cx0rp0orrjcm.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=db3new
+export AWS_BUCKET=cairocoder-udacity-p3
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=cairocoder
+export URL=http://localhost:8080
